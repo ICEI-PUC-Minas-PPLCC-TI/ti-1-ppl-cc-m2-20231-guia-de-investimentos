9b6ccb2fa2d9049804ba9d6f1e0b3e37417154b1
Marcos Paulo Laine <marcospslaine@gmail.com>
2023-07-01 16:20:46 -0300

    alterações

--- codigo/T.I.investCarroussel/T.I.investCarroussel/script.js
+++ codigo/T.I.investCarroussel/T.I.investCarroussel/script.js
@@ -1,7 +1,9 @@
-
 var counter = 1;
+var slideWidth = 25; // Largura de cada slide em porcentagem
+
 var intervalId = setInterval(function() {
   document.getElementById(`radio${counter}`).checked = true;
+  document.querySelector('.slides').style.transform = `translateX(-${slideWidth * (counter - 1)}%)`; // Atualiza a posição dos slides
   counter++;
   if (counter > 4) {
     counter = 1;
